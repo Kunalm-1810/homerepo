0c76333f4e52ebdc894d07f95ae019bf2500841e
kunal home <kode.techm@gmail.com>
2025-12-25 13:04:03 +0000

    v2

--- task_casli.sh
+++ task_casli.sh
@@ -2,7 +2,7 @@
 # Date 10-dec-2025
 # Maintainer "kunal"
 # using function and make a calculator getting values from user
-
+# adding this file to git
 read -p " please provide number1 - " num1
 read -p " please prvider number2 - " num2
 
