e14db82005db4337e16aa49417f62b0ab82294c9
kunal home <kode.techm@gmail.com>
2025-12-25 14:20:20 +0000

    Revert " adding all files"
    This reverts commit e19a2f250212b8e4faf78888dd073d2a15a31dd6.

--- projects/myproject/04_constantvar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Date 07-dec-2025
-#Maintainer "kunal"
-#This script is for constant variable
-
-readonly UNIVERSITY="MUMBAI UNIVERSITY"
-
-echo " I'm from $UNIVERSITY"
-
--- projects/myproject/35_scriptnam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Date 11-dec-2025
-# maintainer "kunal"
-
-echo " Name of the script is ${0}"
-
